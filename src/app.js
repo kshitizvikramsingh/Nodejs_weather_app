dba7b542c4168b05e79ebfe1fa5c481989108e48
kshitizchamp <kshitizvikramsingh123@gmail.com>
2023-03-06 16:15:13 +0530

    changed port to http port 80

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ const geocoding=require("./geocoding.js");
 const forecast=require("./forecast.js");
 
 const app=express();
-const port=process.env.PORT || 3000;
+const port=process.env.PORT || 80;
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"/views"));
 
